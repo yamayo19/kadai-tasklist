1a0b19e87b4918a40db347163b580e3fed3f4626
yosuke.yamamoto <yamayo19@gmail.com>
2021-11-03 20:09:25 +0000

    submit loginlogout

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,7 +12,7 @@ class TasksController < ApplicationController
   end
   
   def create
-        @task = Task.new(task_params)
+        @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'Task が正常に保存されました'
@@ -53,5 +53,10 @@ class TasksController < ApplicationController
   def task_params
     params.require(:task).permit(:content, :status)
   end
-  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   root to: 'tasks#index'
-
+  get 'signup', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   resources :tasks
+  resources :users, only: [:index, :show, :create]
 end
